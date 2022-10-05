fad10a6ae55d478e5fa24042c1dec9a6cf983483
Sangeeth Sadanand <sadanand.sangeeth@gmail.com>
2022-10-05 23:51:50 +0530

    Completed Scrapy for crawling Articles

--- ScrapyFramework/news/news/spiders/hansindia.py
+++ ScrapyFramework/news/news/spiders/hansindia.py
@@ -17,5 +17,10 @@ class HansindiaSpider(scrapy.Spider):
     def parse_section(self,response):
         a = set(response.css("#listing_main_level_top a::attr(href)").getall())
         for link in list(a):
-            yield {"link": link}
+            yield response.follow(link, callback=self.parse_article, cb_kwargs = {'link': link})
             
+    def parse_article(self, response, link):
+        yield {'title': response.css('#detailsContentSectionWrapper > h1::text').get(),
+               'date':  response.css('#detailsContentSectionWrapper span.convert-to-localtime::text').get(),
+               'description': "\n".join(response.css('#details-page-infinite-scrolling-data > div.article-content-data div.story_content > div p::text').getall()),
+               'link': urljoin(HansindiaSpider.start_urls[0], link)}
